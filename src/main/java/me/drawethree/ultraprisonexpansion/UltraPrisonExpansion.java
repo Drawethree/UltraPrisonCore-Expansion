a81b04408fab555ed6b4be0edec1bece1434ba22
Drawethree <janci.kluka@gmail.com>
2021-07-25 23:37:36 +0200

    Up-to-date with 1.4.2-BETA of UltraPrisonCore

--- src/main/java/me/drawethree/ultraprisonexpansion/UltraPrisonExpansion.java
+++ src/main/java/me/drawethree/ultraprisonexpansion/UltraPrisonExpansion.java
@@ -22,19 +22,25 @@ public class UltraPrisonExpansion extends PlaceholderExpansion {
 	private UltraPrisonCore plugin;
 
 	/**
-	 * Because this is a internal class, this check is not needed
-	 * and we can simply return {@code true}
+	 * Because this is an internal class,
+	 * you must override this method to let PlaceholderAPI know to not unregister your expansion class when
+	 * PlaceholderAPI is reloaded
 	 *
-	 * @return Always true since it's an internal class.
+	 * @return true to persist through reloads
 	 */
 	@Override
-	public boolean canRegister(){
-		return (plugin = (UltraPrisonCore) Bukkit.getPluginManager().getPlugin(getRequiredPlugin())) != null;
+	public boolean persist() {
+		return true;
 	}
 
 	@Override
-	public String getRequiredPlugin(){
+	public String getRequiredPlugin() {
 		return "UltraPrisonCore";
+	}
+
+	@Override
+	public boolean canRegister() {
+		return (plugin = (UltraPrisonCore) Bukkit.getPluginManager().getPlugin(this.getRequiredPlugin())) != null;
 	}
 
 	/**
@@ -92,80 +98,120 @@ public class UltraPrisonExpansion extends PlaceholderExpansion {
 			return "";
 		}
 
-
-		if (identifier.equalsIgnoreCase("tokens") || identifier.equalsIgnoreCase("tokens_2")) {
-			return String.format("%,d", plugin.getTokens().getTokensManager().getPlayerTokens(player));
-		} else if (identifier.equalsIgnoreCase("gems") || identifier.equalsIgnoreCase("gems_2")) {
-			return String.format("%,d", plugin.getGems().getGemsManager().getPlayerGems(player));
-		} else if (identifier.equalsIgnoreCase("blocks") || identifier.equalsIgnoreCase("blocks_2")) {
-			return String.format("%,d", plugin.getTokens().getTokensManager().getPlayerBrokenBlocks(player));
-		} else if (identifier.equalsIgnoreCase("multiplier")) {
-			return String.format("%.2f", (1.0 + plugin.getMultipliers().getApi().getPlayerMultiplier(player)));
-		} else if (identifier.equalsIgnoreCase("multiplier_global")) {
-			return String.format("%.2f", plugin.getMultipliers().getApi().getGlobalMultiplier());
-		} else if (identifier.equalsIgnoreCase("rank")) {
-			return plugin.getRanks().getApi().getPlayerRank(player).getPrefix();
-		} else if (identifier.equalsIgnoreCase("next_rank")) {
-			Rank nextRank = plugin.getRanks().getApi().getNextPlayerRank(player);
-			return nextRank == null ? "" : nextRank.getPrefix();
-		} else if (identifier.equalsIgnoreCase("next_rank_cost")) {
-			return String.format("%,.2f", plugin.getRanks().getRankManager().getNextRankCost(player));
-		} else if (identifier.equalsIgnoreCase("prestige")) {
-			return plugin.getRanks().getApi().getPlayerPrestige(player).getPrefix();
-		} else if (identifier.equalsIgnoreCase("autominer_time")) {
-			return plugin.getAutoMiner().getTimeLeft(player);
-		} else if (identifier.equalsIgnoreCase("tokens_formatted") || identifier.equalsIgnoreCase("tokens_3")) {
-			return formatNumber(plugin.getTokens().getTokensManager().getPlayerTokens(player));
-		} else if (identifier.equalsIgnoreCase("gems_formatted")) {
-			return formatNumber(plugin.getGems().getGemsManager().getPlayerGems(player));
-		} else if (identifier.equalsIgnoreCase("rankup_progress")) {
-			return String.format("%d%%", plugin.getRanks().getRankManager().getRankupProgress(player));
-		} else if (identifier.equalsIgnoreCase("tokens_1")) {
-			return String.valueOf(plugin.getTokens().getTokensManager().getPlayerTokens(player));
-		} else if (identifier.equalsIgnoreCase("blocks_1")) {
-			return String.valueOf(plugin.getTokens().getTokensManager().getPlayerBrokenBlocks(player));
-		} else if (identifier.equalsIgnoreCase("blocks_3")) {
-			return formatNumber(plugin.getTokens().getTokensManager().getPlayerBrokenBlocks(player));
-		} else if (identifier.equalsIgnoreCase("gems_1")) {
-			return String.valueOf(plugin.getGems().getGemsManager().getPlayerGems(player));
-		} else if (identifier.equalsIgnoreCase("gems_3")) {
-			return formatNumber(plugin.getGems().getGemsManager().getPlayerGems(player));
-		} else if (identifier.equalsIgnoreCase("pickaxe_level")) {
-
-			PickaxeLevel level = plugin.getPickaxeLevels().getApi().getPickaxeLevel(player);
-
-			if (level != null) {
-				return String.valueOf(level.getLevel());
-			} else {
-				return "0";
+		switch (identifier.toLowerCase()) {
+			case "tokens":
+			case "tokens_2":
+				return String.format("%,d", plugin.getTokens().getTokensManager().getPlayerTokens(player));
+			case "gems":
+			case "gems_2":
+				return String.format("%,d", plugin.getGems().getGemsManager().getPlayerGems(player));
+			case "blocks":
+			case "blocks_2":
+				return String.format("%,d", plugin.getTokens().getTokensManager().getPlayerBrokenBlocks(player));
+			case "multiplier":
+				return String.format("%.2f", (1.0 + plugin.getMultipliers().getApi().getPlayerMultiplier(player)));
+			case "multiplier_global":
+				return String.format("%.2f", plugin.getMultipliers().getApi().getGlobalMultiplier());
+			case "rank":
+				return plugin.getRanks().getApi().getPlayerRank(player).getPrefix();
+			case "next_rank": {
+				Rank nextRank = plugin.getRanks().getApi().getNextPlayerRank(player);
+				return nextRank == null ? "" : nextRank.getPrefix();
 			}
+			case "next_rank_cost":
+				return String.format("%,.2f", plugin.getRanks().getRankManager().getNextRankCost(player));
+			case "next_rank_cost_formatted":
+				return formatNumber(plugin.getRanks().getRankManager().getNextRankCost(player));
+			case "prestige":
+				return plugin.getRanks().getApi().getPlayerPrestige(player).getPrefix();
+			case "prestige_id":
+				return String.valueOf(plugin.getRanks().getApi().getPlayerPrestige(player).getId());
+			case "autominer_time":
+				return plugin.getAutoMiner().getTimeLeft(player);
+			case "tokens_formatted":
+			case "tokens_3":
+				return formatNumber(plugin.getTokens().getTokensManager().getPlayerTokens(player));
+			case "gems_formatted":
+			case "gems_3":
+				return formatNumber(plugin.getGems().getGemsManager().getPlayerGems(player));
+			case "rankup_progress":
+				return String.format("%d%%", plugin.getRanks().getRankManager().getRankupProgress(player));
+			case "tokens_1":
+				return String.valueOf(plugin.getTokens().getTokensManager().getPlayerTokens(player));
+			case "blocks_1":
+				return String.valueOf(plugin.getTokens().getTokensManager().getPlayerBrokenBlocks(player));
+			case "blocks_3":
+				return formatNumber(plugin.getTokens().getTokensManager().getPlayerBrokenBlocks(player));
+			case "gems_1":
+				return String.valueOf(plugin.getGems().getGemsManager().getPlayerGems(player));
+			case "pickaxe_level": {
+				PickaxeLevel level = plugin.getPickaxeLevels().getApi().getPickaxeLevel(player);
 
-		} else if (identifier.equalsIgnoreCase("pickaxe_progress")) {
-			return this.plugin.getPickaxeLevels().getProgressBar(player);
-		} else if (identifier.equalsIgnoreCase("gang_name")) {
-			Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
-			if (optionalGang.isPresent()) {
-				return optionalGang.get().getName();
-			} else {
-				return ChatColor.RED + "✗";
+				if (level != null) {
+					return String.valueOf(level.getLevel());
+				} else {
+					return "0";
+				}
 			}
-		} else if (identifier.equalsIgnoreCase("gang_value")) {
-			Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
-			if (optionalGang.isPresent()) {
-				return String.format("%,d", optionalGang.get().getValue());
-			} else {
+			case "pickaxe_progress":
+				return this.plugin.getPickaxeLevels().getProgressBar(player);
+			case "gang_name":
+			case "gang": {
+				Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
+				if (optionalGang.isPresent()) {
+					return optionalGang.get().getName();
+				} else {
+					return ChatColor.RED + "✗";
+				}
+			}
+			case "gang_value": {
+				Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
+				if (optionalGang.isPresent()) {
+					return String.format("%,d", optionalGang.get().getValue());
+				} else {
+					return "";
+				}
+			}
+			case "gang_has_gang":
+				return this.plugin.getGangs().getGangsManager().getPlayerGang(player).isPresent() ? "Yes" : "No";
+			case "gang_is_leader": {
+				Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
+				if (optionalGang.isPresent()) {
+					return optionalGang.get().isOwner(player) ? "Yes" : "No";
+				}
 				return "";
 			}
+			case "gang_leader_name": {
+				Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
+				if (optionalGang.isPresent()) {
+					return optionalGang.get().getOwnerOffline().getName();
+				}
+				return "";
+			}
+			case "gang_members_amount": {
+				Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
+				if (optionalGang.isPresent()) {
+					return String.valueOf(optionalGang.get().getMembersOffline().size());
+				}
+				return "";
+			}
+			case "gang_members_online": {
+				Optional<Gang> optionalGang = this.plugin.getGangs().getGangsManager().getPlayerGang(player);
+				if (optionalGang.isPresent()) {
+					return String.valueOf(optionalGang.get().getOnlinePlayers().size());
+				}
+				return "";
+			}
+			default:
+				return null;
 		}
-		return null;
 	}
 
-	public static String formatNumber(double amount) {
+	static String formatNumber(double amount) {
 		if (amount <= 1000.0D)
 			return String.valueOf(amount);
-		ArrayList<String> suffixes = new ArrayList<>(Arrays.asList(new String[]{
-				"", "k", "M", "B", "T", "q", "Q", "QT", "S", "SP", "O",
-				"N", "D"}));
+		ArrayList<String> suffixes = new ArrayList<>(Arrays.asList("", "k", "M", "B", "T", "q", "Q", "QT", "S", "SP", "O",
+				"N", "D"));
 		double chunks = Math.floor(Math.floor(Math.log10(amount) / 3.0D));
 		amount /= Math.pow(10.0D, chunks * 3.0D - 1.0D);
 		amount /= 10.0D;
